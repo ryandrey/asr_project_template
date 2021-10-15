c1b753fc9fa0016fdffea9732f22fd23e3667e47
ryandrey <r.andrew200@mail.ru>
2021-10-15 15:19:51 +0300

    lstm model

--- hw_asr/model/baseline_model.py
+++ hw_asr/model/baseline_model.py
@@ -21,3 +21,18 @@ class BaselineModel(BaseModel):
 
     def transform_input_lengths(self, input_lengths):
         return input_lengths  # we don't reduce time dimension here
+
+
+class MyLSTM(BaseModel):
+    def __init__(self, n_feats, n_class, n_layers=3, fc_hidden=512, *args, **kwargs):
+        super().__init__(n_feats, n_class, *args, **kwargs)
+        self.encoder = nn.LSTM(n_feats, fc_hidden, batch_first=True, num_layers=n_layers)
+        self.head = nn.Linear(fc_hidden, n_class)
+
+    def forward(self, spectrogram, *args, **kwargs):
+        x, _ = self.encoder(spectrogram)
+        x = self.head(x)
+        return x
+
+    def transform_input_lengths(self, input_lengths):
+        return input_lengths
